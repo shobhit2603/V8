284920a41177e066b5e9252905467324684e5863
Shobhit Shrivastava <shrivastavashobhit706@gmail.com>
2026-05-23 11:35:28 +0530

    Agent setup completed

--- sandbox/template/next.config.mjs
+++ sandbox/template/next.config.mjs
@@ -2,6 +2,13 @@
 const nextConfig = {
   /* config options here */
   reactCompiler: true,
+  webpack: (config) => {
+    config.watchOptions = {
+      poll: 300,
+      aggregateTimeout: 300,
+    };
+    return config;
+  },
 };
 
 export default nextConfig;
