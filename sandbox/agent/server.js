c3ecd4e20e6629115e140f87a7adc0c9a9772214
Shobhit Shrivastava <shrivastavashobhit706@gmail.com>
2026-05-23 11:42:43 +0530

    Agent setup completed

--- sandbox/agent/server.js
+++ sandbox/agent/server.js
@@ -1,7 +1,7 @@
 import app from './src/app.js';
 import 'dontenv/config';
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
